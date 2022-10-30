fba55980d581e1373d034b28ebc9948a633368de
Anita Nofianti <115064588+Whalienn@users.noreply.github.com>
2022-10-30 17:33:03 +0700

    Add files via upload

---
+++ tugas 6 oop/Managerberaksi.java
@@ -0,0 +1,13 @@
+public class Managerberaksi {
+    public static void main(String[] args){
+
+
+        Manager ale = new Manager();
+
+        ale.setNama ("ale");
+        ale.setgajiPokok (5000000);
+        ale.setTunjangan (3000000);
+
+        ale.cetakInfo();
+    }
+}
---
+++ tugas 6 oop/Programmerberaksi.java
@@ -0,0 +1,13 @@
+public class Programmerberaksi {
+    public static void main(String[] args){
+
+
+        Programmer leo = new Programmer();
+
+        leo.setNama ("leo");
+        leo.setgajiPokok (5000000);
+        leo.setBonus (8000000);
+
+        leo.cetakInfo();
+    }
+}
